574cca8174661c3f9e3f7c21d793da4c8c5f772e
MohamedCK <abdulhakimm280@gmail.com>
2023-01-27 15:10:23 +0200

    - Set the validtions for the `like.rb` file

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,6 +3,8 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   before_save :like_counter
 
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+
   private
 
   def like_counter
