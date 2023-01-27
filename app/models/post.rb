c61a7744faf39b0185957ed86a238cfd10c3c2f0
MohamedCK <abdulhakimm280@gmail.com>
2023-01-27 19:47:26 +0200

    - Fix Linter Errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
-  
+
   def latest_comments
     comments.order(created_at: :desc).limit(5)
   end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   context 'Tests For #Comment Model' do
     before :each do
-      @user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+      @user = User.create(name: 'King Cake',
+                          photo: 'https://media.istockphoto.com/id/650966412/photo/homemade-colorful-mardi-gras-king-cake.jpg?s=612x612&w=0&k=20&c=l2xfuI8SDGwDWOcbPvZz7wcCSMd0-IXOv03BJk-zSys=', bio: 'It is the ultimate the most deliciouse the jaguar King Cake.')
 
       @post = Post.create(author: @user, title: 'Hello', text: 'This is my first post')
 
