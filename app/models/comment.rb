f2bcf5b5ca5b3c96ae61bba4ebf2d1f3dd80285e
MohamedCK <abdulhakimm280@gmail.com>
2023-01-27 15:09:21 +0200

    - Set the validtions for `comment.rb`

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,8 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   before_save :comment_counter
 
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
+  
   private
 
   def comment_counter
