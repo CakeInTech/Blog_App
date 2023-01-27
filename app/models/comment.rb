bfa758519880525d1804b5fd7ded3f5f72e137ba
MohamedCK <abdulhakimm280@gmail.com>
2023-01-27 19:23:14 +0200

    - Update validations for models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,13 +1,13 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  before_save :comment_counter
+  before_save :comments_counter
 
   validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
-  
+
   private
 
-  def comment_counter
+  def comments_counter
     post.update(comments_counter: post.comments.all.length)
   end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,13 +1,13 @@
 class Like < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  before_save :like_counter
+  before_save :likes_counter
 
   validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
 
   private
 
-  def like_counter
+  def likes_counter
     post.update(likes_counter: post.likes.all.length)
   end
 end
