dea680212ce78882571764f1feb1de84bf0066ed
MohamedCK <abdulhakimm280@gmail.com>
2023-01-27 15:26:53 +0200

    - Write a unit test for `user_test.rb` file

--- Gemfile
+++ Gemfile
@@ -3,6 +3,10 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.1.3'
 
+# RSpec testing library
+gem 'rspec-rails'
+gem 'rspec-support'
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.0.4', '>= 7.0.4.1'
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
-require 'test_helper'
+require 'rails_helper'
 
-class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+RSpec.describe User, type: :model do 
+  describe 'validations' do 
+    it { should validate_presence_of(:name)}
+    it { should validate_numericality_of(:posts_counter).is_greater_than_or_equal_to(0) }
+  end
 end
