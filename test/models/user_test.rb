1333c48bd3faa675824769cff9741ef22bbd93d1
MohamedCK <abdulhakimm280@gmail.com>
2023-01-27 19:27:55 +0200

    - Move unit test files to the spec folder

--- test/models/user_test.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe User, type: :model do 
-  describe 'validations' do 
-    it { should validate_presence_of(:name)}
-    it { should validate_numericality_of(:posts_counter).is_greater_than_or_equal_to(0) }
-  end
-end
