9048bf486ebd3645c7ffc8d5262da7d4c2ded2fb
MohamedCK <abdulhakimm280@gmail.com>
2023-02-02 12:13:28 +0200

    - Implement resourceful routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,8 @@ Rails.application.routes.draw do
    root "users#index"
   # Defines the root path route ("/")
   # root "articles#index"
-  # Users
-  get 'users/:id' => 'users#show'
-
-  # Posts
-  get 'users/:user_id/posts' => 'posts#index'
-  get 'users/:user_id/posts/:id' => 'posts#show'
+  root 'users#index' 
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
