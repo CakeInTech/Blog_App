9ac70b5b95035179648397c187212d907ba0978d
MohamedCK <abdulhakimm280@gmail.com>
2023-02-11 00:10:58 +0200

    - Add index action for post controller in API V1

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::PostsController < ApplicationController
+
+    def index 
+        user = User.find(params[:user_id])
+        post = user.post
+        render json: post
+    end
+end
